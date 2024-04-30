d64bc75dc38143991fccf124191abd361edbf73f
Marc Christenfeldt <marc.christenfeldt@gmx.de>
2024-04-30 10:50:22 +0200

    Load configuration from file in main function.

--- main.go
+++ main.go
@@ -3,11 +3,35 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
+type Config struct {
+	Repositories []string
+}
+
+func LoadConfig(filename string) (Config, error) {
+	var config Config
+	file, err := os.Open(filename)
+	if err != nil {
+		return config, err
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		return config, err
+	}
+	return config, nil
+}
+
+var config Config
+
 func main() {
-	config, err := LoadConfig("config.json")
+	var err error
+	config, err = LoadConfig("config.json")
 	if err != nil {
 		log.Fatalf("Failed to load config: %s", err)
 	}
@@ -24,6 +48,7 @@ func main() {
 	fmt.Println("http://localhost:8080/repositories")
 	http.ListenAndServe(":8080", nil)
 }
+
 func getRepositories(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(config.Repositories)
