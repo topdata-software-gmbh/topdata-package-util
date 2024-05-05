b1877ec20fa8a66cf73edeec76da0e557e79ded3
Marc Christenfeldt <marc.christenfeldt@gmx.de>
2024-05-05 14:38:52 +0200

    zip filename construction works

--- commands/pkg_commands/pkg_build_release_zip_command.go
+++ commands/pkg_commands/pkg_build_release_zip_command.go
@@ -4,6 +4,7 @@ import (
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 	"github.com/topdata-software-gmbh/topdata-package-service/config"
+	"github.com/topdata-software-gmbh/topdata-package-service/factory"
 	"github.com/topdata-software-gmbh/topdata-package-service/git_cli_wrapper"
 )
 
@@ -12,21 +13,27 @@ var buildReleaseZipCommand = &cobra.Command{
 	Short: "Builds a release zip for uploading to the shopware6 plugin store",
 	Args:  cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		pathPackagesPortfolioFile, _ := cmd.Flags().GetString("packages-portfolio-file")
-		pkgConfigList := config.LoadPackagePortfolioFile(pathPackagesPortfolioFile)
-		pkgConfig := pkgConfigList.FindOneByNameOrFail(args[0])
-
 		packageName := args[0]
 		releaseBranchName := args[1]
-		color.Blue("TODO: Implement build-release-zip command for package %s and release branch %s", packageName, releaseBranchName)
+
+		pathPackagesPortfolioFile, _ := cmd.Flags().GetString("packages-portfolio-file")
+		pkgConfigList := config.LoadPackagePortfolioFile(pathPackagesPortfolioFile)
+		pkgConfig := pkgConfigList.FindOneByNameOrFail(packageName)
+
+		gitBranchInfo := factory.NewGitBranchInfo(*pkgConfig, releaseBranchName)
 
 		// -- switch to the release branch
 		git_cli_wrapper.SwitchBranch(*pkgConfig, releaseBranchName)
-		//  -- update local git repository
-		// git_cli_wrapper.UpdateRepo(*pkgConfig)
 
-		//  create a zip file
-		//  upload the zip file to the shopware6 plugin store
+		//  -- update local git repository
+		// TODO... git_cli_wrapper.UpdateRepo(*pkgConfig)
+
+		//  -- create a zip file
+		pathDestZipFile := "/tmp/" + pkgConfig.Name + "-" + gitBranchInfo.PackageVersion + ".zip"
+		color.Blue("Creating zip file " + pathDestZipFile + "...")
+
+		// -- upload the zip file to the shopware6 plugin store
+
 	},
 }
 
--- util/string_slices.go
+++ util/string_slices.go
@@ -5,6 +5,7 @@ import (
 	"regexp"
 )
 
+// FilterStringSlice filters the given strings based on the given regex pattern and returns only those that match/match not the pattern.
 func FilterStringSlice(strings []string, regexPattern string, keepMatches bool) []string {
 	filtered := make([]string, 0)
 	for _, str := range strings {
@@ -20,8 +21,6 @@ func FilterStringSlice(strings []string, regexPattern string, keepMatches bool) 
 	return filtered
 }
 
-// FilterStringSlicePositive filters the given branches and returns only those that are either "server" or start with "release-".
-// It takes a slice of strings representing the branch names as input and returns a slice of strings containing the filtered branch names.
 func FilterStringSlicePositive(branches []string, regexPattern string) []string {
 	return FilterStringSlice(branches, regexPattern, true)
 }
@@ -30,7 +29,7 @@ func FilterStringSliceNegative(names []string, regex string) []string {
 	return FilterStringSlice(names, regex, false)
 }
 
-// contains checks if a string slice contains a string
+// StringSliceContains checks if a string slice contains a string
 func StringSliceContains(slice []string, item string) bool {
 	for _, a := range slice {
 		if a == item {
